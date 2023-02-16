b173673e88ed3dcae1b92e517f1c2eea1302dd70
nahinmahmood0101 <nahin.mahmood27@myhunter.cuny.edu>
2023-02-16 14:27:24 -0500

    Update splash_screen.dart
    script

--- lib/splashScreen/splash_screen.dart
+++ lib/splashScreen/splash_screen.dart
@@ -28,7 +28,7 @@ class _MySplashScreenState extends State<MySplashScreen> {
   Widget build(BuildContext context) {
     return Material(
         child: Container(
-            color: Colors.green,
+            color: Colors.brown,
             child: Center(
                 child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
