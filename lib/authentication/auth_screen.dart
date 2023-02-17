74f64508e39cf61f5aba4c88188961a1de30e278
nahinmahmood0101 <nahin.mahmood27@myhunter.cuny.edu>
2023-02-16 19:14:10 -0500

    hi

--- lib/authentication/auth_screen.dart
+++ lib/authentication/auth_screen.dart
@@ -1,18 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:hunger_box/authentication/login.dart';
+import 'package:hunger_box/authentication/registration.dart';
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
 
   @override
-  State<AuthScreen> createState() => _AuthScreenState();
+  _AuthScreenState createState() => _AuthScreenState();
 }
 
 class _AuthScreenState extends State<AuthScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-      title: const Text("Welcome"),
-    ));
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+            appBar: AppBar(
+              flexibleSpace: Container(
+                decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                  colors: [
+                    Colors.blue,
+                    Colors.red,
+                  ],
+                  begin: FractionalOffset(0.0, 0.0),
+                  end: FractionalOffset(1.0, 0.0),
+                  stops: [0.0, 0.1],
+                  tileMode: TileMode.clamp,
+                )),
+              ),
+              title: const Text(
+                "HungerBox",
+                style: TextStyle(
+                  fontSize: 60,
+                  color: Colors.white,
+                ),
+              ),
+              centerTitle: true,
+              bottom: const TabBar(
+                tabs: [
+                  Tab(
+                    icon: Icon(Icons.lock, color: Colors.blue),
+                    text: "Login",
+                  ),
+                  Tab(
+                    icon: Icon(
+                      Icons.lock,
+                      color: Colors.white,
+                    ),
+                    text: "Register",
+                  ),
+                ],
+                indicatorColor: Colors.black45,
+                indicatorWeight: 8,
+              ),
+            ),
+            body: Container(
+              decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+                  Colors.red,
+                  Colors.blue,
+                ],
+              )),
+              child: const TabBarView(
+                children: [
+                  LoginScreen(),
+                  RegisterScreen(),
+                ],
+              ),
+            )));
   }
 }
